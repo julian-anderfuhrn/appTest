384faaa37a638a9123f5bc3a7f0b6cac78693d94
HoudiniDev <74371270+julian-anderfuhrn@users.noreply.github.com>
2023-07-20 02:24:40 -0300

    4ta entrega

---
+++ src/navigations/shop.jsx
@@ -0,0 +1,71 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import { Categories, Products, ProductDetail } from '../screens';
+import { COLORS, FONTS } from '../themes';
+const Stack = createNativeStackNavigator();
+
+function ShopNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Categories"
+      screenOptions={() => ({
+        headerStyle: {
+          backgroundColor: COLORS.primary,
+          height: 80,
+        },
+        headerTitleStyle: {
+          fontFamily: FONTS.bold,
+          fontSize: 16,
+        },
+        headerTintColor: COLORS.white,
+        animation: 'fade_from_bottom',
+      })}>
+      <Stack.Screen name="Categories" component={Categories} />
+      <Stack.Screen
+        name="Products"
+        component={Products}
+        options={({ navigation, route }) => ({
+          headerStyle: {
+            backgroundColor: route.params.color,
+          },
+          headerLeft: () => (
+            <TouchableOpacity style={styles.goBack} onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back-circle" size={30} color={COLORS.white} />
+            </TouchableOpacity>
+          ),
+          title: route.params.name,
+        })}
+      />
+      <Stack.Screen
+        name="ProductDetail"
+        component={ProductDetail}
+        options={({ navigation, route }) => ({
+          headerStyle: {
+            backgroundColor: route.params.color,
+          },
+          headerLeft: () => (
+            <TouchableOpacity style={styles.goBack} onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back-circle" size={30} color={COLORS.white} />
+            </TouchableOpacity>
+          ),
+          title: route.params.name,
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  goBack: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  goBackText: {
+    fontSize: 14,
+    color: COLORS.text,
+  },
+});
+
+export default ShopNavigator;
