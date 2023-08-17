fd7d25c87cee9ad8bb4607effaeb6421edfc15af
HoudiniDev <74371270+julian-anderfuhrn@users.noreply.github.com>
2023-08-16 23:49:35 -0300

    agrego apikey

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,5 +1,5 @@
-export const URL_BASE_FIREBASE_REALTIME_DATABASE = '';
-export const FIREBASE_API_KEY = '';
+export const URL_BASE_FIREBASE_REALTIME_DATABASE = 'https://apptest-15384-default-rtdb.firebaseio.com';
+export const FIREBASE_API_KEY = 'AIzaSyD0hOTvtmPN2P9kIRQ5mSS9x8fN6Jtv4Xc';
 export const FIREBASE_AUTH_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts';
 export const FIREBASE_AUTH_SIGN_IN_API_URL = `:signInWithPassword?key=${FIREBASE_API_KEY}`;
 export const FIREBAS_AUTH_SIGN_UP_API_URL = `:signUp?key=${FIREBASE_API_KEY}`;
